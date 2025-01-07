8b7436cbfeb0458501c23427a1eab3c7443f69a6
Htun <zczhao221@gmail.com>
2025-01-08 02:22:20 +0800

    Delete midterm directory

--- midterm/carpark_system/carpark/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Carpark, CarparkType, ParkingSystem
-
-admin.site.register(Carpark)
-admin.site.register(CarparkType)
-admin.site.register(ParkingSystem)
