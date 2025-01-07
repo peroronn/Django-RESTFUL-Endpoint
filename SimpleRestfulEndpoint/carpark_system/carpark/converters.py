1cfab5091b7c6bc034ff8c72d3eb49ffa60293f6
Htun <zczhao221@gmail.com>
2025-01-08 02:55:32 +0800

    Delete SimpleRestfulEndpoint directory

--- SimpleRestfulEndpoint/carpark_system/carpark/converters.py
+++
@@ -1,8 +0,0 @@
-class FloatConverter:
-    regex = r"-?\d+(\.\d+)?"  # Matches integers or floats
-
-    def to_python(self, value):
-        return float(value)
-
-    def to_url(self, value):
-        return str(value)
