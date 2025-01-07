a657cea29efa5b80b11c17510ce4f0df46590398
Htun <zczhao221@gmail.com>
2025-01-08 02:24:36 +0800

    Delete Simple django Restful endpoint directory

--- Simple django Restful endpoint/carpark_system/carpark/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Carpark, CarparkType
-
-class CarparkSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Carpark
-        fields = '__all__'
-
-        
